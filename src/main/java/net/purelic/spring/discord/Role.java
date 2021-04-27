4b6d72428877c4531b2ec92d090ecea7e5b9db2c
WithoutRegret <mcwithoutregret@gmail.com>
2021-04-27 09:53:04 -0700

    Add more reaction roles

--- src/main/java/net/purelic/spring/discord/Role.java
+++ src/main/java/net/purelic/spring/discord/Role.java
@@ -9,6 +9,9 @@ public class Role {
     public static final String VERIFIED = "832151395112058890";
     public static final String BETA_TESTER = "830694257392091147";
     public static final String LOOKING_TO_PLAY = "830673260952944660";
+    public static final String ANNOUNCEMENTS = "835997424900112384";
+    public static final String MOVIES = "835989590740697098";
+    public static final String MUSIC = "835989682953256960";
     public static final String MUTED = "818950420931805235";
 
     public static String any(String... roles) {
--- src/main/java/net/purelic/spring/listeners/discord/ReactionRoles.java
+++ src/main/java/net/purelic/spring/listeners/discord/ReactionRoles.java
@@ -12,12 +12,20 @@ import java.util.HashMap;
 public class ReactionRoles extends ListenerAdapter {
 
     private static final String ALERT_MESSAGE = "830674462323507210";
+    private static final String CLUB_MESSAGE = "836002343307837530";
+
     private final HashMap<String, String> alertRoles;
+    private final HashMap<String, String> clubRoles;
 
     public ReactionRoles() {
         this.alertRoles = new HashMap<>();
         this.alertRoles.put("\uD83D\uDD28", Role.BETA_TESTER); // 🔨
         this.alertRoles.put("\uD83D\uDD14", Role.LOOKING_TO_PLAY); // 🔔
+        this.alertRoles.put("\uD83D\uDCE3", Role.ANNOUNCEMENTS); // 📣
+
+        this.clubRoles = new HashMap<>();
+        this.clubRoles.put("\uD83C\uDFAC", Role.MOVIES); // 🎬
+        this.clubRoles.put("\uD83C\uDFB5", Role.MUSIC); // 🎵
     }
 
     @Override
@@ -31,6 +39,12 @@ public class ReactionRoles extends ListenerAdapter {
 
         if (messageId.equals(ALERT_MESSAGE)) {
             String roleId = this.alertRoles.get(emoji);
+
+            if (roleId != null) {
+                DiscordUtils.addRole(event.getUser(), roleId).queue();
+            }
+        } else if (messageId.equals(CLUB_MESSAGE)) {
+            String roleId = this.clubRoles.get(emoji);
 
             if (roleId != null) {
                 DiscordUtils.addRole(event.getUser(), roleId).queue();
@@ -49,6 +63,12 @@ public class ReactionRoles extends ListenerAdapter {
 
         if (messageId.equals(ALERT_MESSAGE)) {
             String roleId = this.alertRoles.get(emoji);
+
+            if (roleId != null) {
+                DiscordUtils.removeRole(event.getUser(), roleId).queue();
+            }
+        } else if (messageId.equals(CLUB_MESSAGE)) {
+            String roleId = this.clubRoles.get(emoji);
 
             if (roleId != null) {
                 DiscordUtils.removeRole(event.getUser(), roleId).queue();
